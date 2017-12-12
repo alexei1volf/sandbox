64d39fb2888b918a83e4900d1e95a84eee748bcb
volfav <a.volf@enbisys.com>
2017-12-12 10:21:06 +0700

    add gradle settings

--- web/src/main/java/com/sandbox/testService.java
+++ web/src/main/java/com/sandbox/testService.java
@@ -1,14 +1,19 @@
 package com.sandbox;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @Path("helloworld")
+@Api(description = "Helloworld REST endpoint")
 public class testService {
 
     @GET
     @Produces("text/plain")
+    @ApiOperation(value = "say hello")
     public String getHello() {
         return "Hello World!";
     }
